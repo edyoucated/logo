36bc0cf40720a1ca75c9d3377adf82a98494c156
janpapenbrock <j.papenbrock@hochzehn.com>
2020-09-11 16:31:58 +0200

    Fix: Image density should be set appropriately for the input file.

--- bin/dist.sh
+++ bin/dist.sh
@@ -85,7 +85,7 @@ function createLogoVersions() {
 
 function resize() {
   size=$1
-  inputDensity=$((size * 4))
+  inputDensity=$((size * 2))
   outputDensity=$((72 * 2))
   echo -n "    $size"x...
   docker run --rm --volume "$PWD/$SRC_DIR/fonts/lato":"/usr/share/fonts/truetype/lato" --volume "$PWD/$SRC_SVG":"/app/input.svg" --volume "$PWD/$TARGET_DIR":"/output" --workdir "/app" jujhars13/docker-imagemagick sh -c "fc-cache -f; convert -background none -resize \"$size\"x -density \"$inputDensity\" -depth 8 input.svg  -density \"$outputDensity\" /output/\"$LOGO_TYPE\"-\"$size\".png"
